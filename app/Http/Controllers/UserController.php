b858bccb728ecbd63f9527a97466fd8f5bdb0a93
oldcreatrix <rahulideasetters@gmail.com>
2021-06-12 02:28:24 +0530

    Rough login feature complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use\Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -9,6 +9,12 @@ class UserController extends Controller
 {
     function login(Request $req){
         //return $req->input();
-        return User::where(['email'=>$req->email])->first();
+        $user_r =  User::where(['email'=>$req->email])->first();
+        if(!$user_r || !Hash::check($req->password, $user_r->password)){
+            return "Username or password not matched";
+        }else{
+            $req->session()->put('user', $user_r);
+            return redirect('/');
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::get('/login', function(){
 
 //Route::view("/", "login");
 Route::post("/login", [UserController::class, 'login']);
+Route::get('/', [ProductController::class, 'index']);
